b55e6559d517f04d64cc0410e4becb0d2d6058e9
minam1994 <40855189+minam1994@users.noreply.github.com>
2018-09-30 11:31:55 +0200

    adding project

---
+++ home.php
@@ -0,0 +1,31 @@
+<DOCTYPE html>
+<html>
+<head>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link href="responsive.css" rel="stylesheet">
+
+
+    
+</head>
+<body>
+
+    <div class="center">
+        <h1>Buy desserts:</h1>
+        <form class="form" method="get" action="index.php">
+
+            <input type="checkbox" id="check1" class="box" name="item[]" value="1"><label for="check1">Cheesecake - 10$</label><br>
+            <input type="checkbox" id="check2" name="item[]" value="2"><label for="check2">Chocolate cake - 8$</label><br>
+            <input type="checkbox" id="check3" name="item[]" value="3"><label for="check3">Muffin - 5$</label><br>
+            <input type="checkbox" id="check4" name="item[]" value="4"><label for="check4">Vanilla cake - 7$</label><br>
+
+            <button class="btn" type="submit">Order</button>
+        </form>
+    </div>
+    <footer>
+        <small>&copy;<?php echo date('Y'); ?> - Mina Maksic</small>
+    </footer>
+
+
+</body>
+</html>
